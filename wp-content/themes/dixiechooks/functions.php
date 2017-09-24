dce0dd94de6a61943500d6ad3efcbf6b3b339c9c
Sebastian Key <sebastianwkey@gmail.com>
2017-09-24 19:52:10 +1000

    first commit

---
+++ wp-content/themes/dixiechooks/functions.php
@@ -0,0 +1,19 @@
+<?php
+function wpb_add_google_fonts() {
+
+wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Cormorant:700', false );
+}
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'morphology-lite';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles');
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts');
+?>
